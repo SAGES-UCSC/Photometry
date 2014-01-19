e7009911ceebe70a8ae190eb76265bc61ec903df
Alexa Villaume <avillaum@ucsc.edu>
2014-01-19 12:31:28 -0800

    Making this object oriented

---
+++ Sources.py
@@ -0,0 +1,18 @@
+class CFHTSource:
+    # Take source line, split(), float them,
+    def __init__(self, line):
+        self.line = line
+        cols = line.split()
+        self.name = cols[0]
+        self.ra = float(cols[1])
+        self.dec = float(cols[2])
+        self.mag1 = float(cols[3])
+        self.mag2 = float(cols[5])
+        self.mag3 = float(cols[7])
+        self.mag4 = float(cols[9])
+        self.mag1_err = float(cols[4])
+        self.mag2_err = float(cols[6])
+        self.mag3_err = float(cols[8])
+        self.mag4_err = float(cols[10])
+        self.a_world = float(cols[11])
+        self.b_world = float(cols[12])
