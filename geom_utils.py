0717cea5fbcddcc240e8cd773933023523dca373
Alexa Villaume <avillaum@ucsc.edu>
2014-02-09 13:56:38 -0800

    More quadtree geometry

--- geom_utils.py
+++ geom_utils.py
@@ -17,7 +17,17 @@ def inParallelogram(px, py,  m, b, var):
     return is_in
 
 '''
+All of these inputs need to be in degrees.
+'''
+def makeRadiusCut(ra, dec, gal_ra, gal_dec, distance):
+    if (math.sqrt((ra-gal_ra)**2) + math.sqrt((dec-gal_dec)**2)) <= distance:
+        return 1
+    else:
+        return 0
+
+'''
 Specifically for use with the quadtree
+Takes two Box objects as arguments
 '''
 def intersecting(b1, b2):
     if (b1.xmin >= b2.xmin and b1.xmin < b2.xmax) or
@@ -45,11 +55,23 @@ def norm2(x1, y1, x2, y2):
     yd = y2 -y1
     return xd * x2 + yd * yd
 
-'''
-All of these inputs need to be in degrees.
-'''
-def makeRadiusCut(ra, dec, gal_ra, gal_dec, distance):
-    if (math.sqrt((ra-gal_ra)**2) + math.sqrt((dec-gal_dec)**2)) <= distance:
-        return 1
+def dblmax(a, b):
+    if a > b:
+        return a
     else:
-        return 0
+        return b
+
+def dblmin(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+'''
+Specifically for use with the quadtree
+Takes two Box objects as arguments
+'''
+def clip_box(b, bounds):
+    b.xmin = dblmax(b.xmin, bounds.xmin)
+    b.ymin = dblmax(b.ymin, bounds.ymin)
+    b.xmax = dblmin(b.xmax, bounds.xmax)
+    b.ymax = dblmin(b.ymax, bounds.ymax)
