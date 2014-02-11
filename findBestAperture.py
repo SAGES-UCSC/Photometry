9304df81c57dcdd407a5a41fd3dd0a5143091906
Alexa Villaume <avillaum@ucsc.edu>
2014-02-11 12:48:29 -0800

    Working out the final touches still. Runs but not functioning correctly

--- findBestAperture.py
+++ findBestAperture.py
@@ -15,7 +15,7 @@ from subprocess import call
 import Quadtree as q
 from pprint import pprint
 
-MAXDIST = 0.5
+MAXDIST = 10
 
 '''
 In this case we want the objects that DON'T match
@@ -25,7 +25,8 @@ def disassociate(list1, tree2):
     while list1:
         target = list1.pop()
         match2 = tree2.match(target.ximg, target.yimg)
-        if match2 != None and norm2(match2.ximg, match2.yimg, target.ximg, target.yimg) > MAXDIST:
+        if match2 != None and norm2(match2.ximg, match2.yimg, target.ximg, target.yimg) < MAXDIST:
+            print target
             unmatched.append(target)
 
 def main():
@@ -35,11 +36,8 @@ def main():
     filter_file = "default.conv"
 
     output = open("MeasureFluxAt.txt", "w")
-    for i in range(0,10000):
+    for i in range(0,100000):
         output.write('%.3f' % r.uniform(1,10000) + '%10.3f' % r.uniform(1,8000) + '\n')
-
-    aperture = np.linspace(0.5, 10, num=1)
-    #noise = gn.getNoise(aperture, name, filter_file, image,  False)
 
     sp.createSexParam(sname, False)
     sp.createSexParam(nname, True)
@@ -49,38 +47,42 @@ def main():
 
     signal = []
     noise = []
+    aperture = np.linspace(0.5, 10, num=1)
     for ap in aperture:
-        #sc.createSexConfig(sname, filter_file, sparam_file, "nill", ap, False)
-        #call(['sex', '-c', sname + '.config', image])
+        sc.createSexConfig(sname, filter_file, sparam_file, "nill", ap, False)
+        call(['sex', '-c', sname + '.config', image])
 
-        #sc.createSexConfig(nname, filter_file, nparam_file, assoc_file, ap, True)
-        #call(['sex', '-c', nname + '.config', image])
+        sc.createSexConfig(nname, filter_file, nparam_file, assoc_file, ap, True)
+        call(['sex', '-c', nname + '.config', image])
 
         scat = open(sname + ".cat")
         stmp = filter(lambda line: pu.noHead(line), scat)
-        #ssources = map(lambda line: S.SCAMSource(line), stmp)
-        #sflux = map(lambda s: s.mag_aper, ssources)
-        #signal.append(pu.calcMAD(sflux))
 
         ncat = open(nname + ".cat")
         ntmp = filter(lambda line: pu.noHead(line), ncat)
-        #nflux = map(lambda s: s.mag_aper, nsources)
-        #noise.append(pu.calcMAD(nflux))
 
-        # Make sure that the background measuresments don't overlap with the source detections
+        # Background measuresments can't overlap with source detections
         # Also don't include mag_aper == 99.0
         ssources = q.Quadtree(0, 0, 10000, 8000)
         map(lambda line: ssources.insert(S.SCAMSource(line)), stmp)
         nsources = map(lambda line: S.SCAMSource(line), ntmp)
 
-        matchlist = disassociate(nsources, ssources)
+        bkgddetections = disassociate(nsources, ssources)
 
-    snr = []
-    for i in range(len(signal)):
-        snr.append(signal[i]/noise[i])
+        #sflux = map(lambda s: s.mag_aper, ssources)
+        #signal.append(pu.calcMAD(sflux))
 
-    plt.plot(aperture, snr, linestyle='none', marker='o')
-    plt.show()
+        #nflux = map(lambda s: s.mag_aper, bkgddetections)
+        #noise.append(pu.calcMAD(nflux))
+
+
+#    snr = []
+#    for i in range(len(signal)):
+#        snr.append(signal[i]/noise[i])
+
+#    plt.plot(aperture, sflux, linestyle='none', marker='o')
+#    plt.plot(aperture, nflux, linestyle='none', marker='o')
+#    plt.show()
 
 if __name__ == '__main__':
     sys.exit(main())
