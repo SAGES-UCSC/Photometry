c261781b914555ddab107642e75a07cb8970b05d
Alexa Villaume <avillaum@ucsc.edu>
2014-02-13 19:58:37 -0800

    fixed subdivide problem

--- findBestAperture.py
+++ findBestAperture.py
@@ -63,10 +63,8 @@ def main():
 
         # Background measuresments can't overlap with source detections
         # Also don't include mag_aper == 99.0
-        ssources = q.Quadtree(0, 0, 10000, 8000)
-        for i in range(0,100):
-            ssources.insert(S.SCAMSource(stmp[i]))
-        #map(lambda line: ssources.insert(S.SCAMSource(line)), stmp)
+        ssources = q.Quadtree(0.0, 0.0, 11000.0, 9000.0)
+        map(lambda line: ssources.insert(S.SCAMSource(line)), stmp)
         nsources = map(lambda line: S.SCAMSource(line), ntmp)
 
         bkgddetections = disassociate(nsources, ssources)
