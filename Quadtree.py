724bcfc1780b32842c6c4f652b015beab9b73dac
Alexa Villaume <avillaum@ucsc.edu>
2014-02-16 20:28:53 -0800

    Hey it works now

--- Quadtree.py
+++ Quadtree.py
@@ -5,7 +5,7 @@ import geom_utils as gu
 import math
 
 MAX = 12
-verbose = False
+verbose = True
 
 class Quadtree:
     def __init__(self, xmin, ymin, xmax, ymax):
@@ -48,37 +48,28 @@ class Quadtree:
             self.inserttoquad(node, node.contents.pop())
 
     def match(self, x, y):
-        self.nearestsource(self, x, y)
+        return self.nearestsource(self, x, y)
+
 
     def nearestsource(self, tree, x, y):
         nearest = {'source':None, 'dist':0}
 
         # Initialize a box of interest
         nearest['dist'] = min(tree.top.xmax - tree.top.xmin, tree.top.ymax - tree.top.ymin) / 1000.0
-        interest = {'xmin':x-dist, 'ymin':y-dist, 'xmax':x+dist, 'ymax':y+dist,}
+        interest = {'xmin':x-nearest['dist'], 'ymin':y-nearest['dist'], 'xmax':x+nearest['dist'], 'ymax':y+nearest['dist']}
         interest = gu.clip_box(interest['xmin'], interest['ymin'], interest['xmax'], interest['ymax'],
                     tree.top.xmin, tree.top.ymin, tree.top.xmax, tree.top.ymax)
         nearest['dist'] = nearest['dist'] * nearest['dist']
 
-        if verbose:
-            print "nearest_source"
-            print "     target", x, y
-            print "     interest", interest['xmin'], interest['ymin'], interest['xmax'], interest['ymax']
 
         self.nearersource(tree, tree.top, x, y, nearest, interest)
-        if verbose:
-            print "Nearestsource receiving", nearest
 
+        #print "From Nearestsource ", nearest['source']
         return nearest['source']
 
     def nearersource(self, tree, node, x, y, nearest, interest):
-        if verbose:
-            print "nearer source", node.xmin, node.ymin, node.xmax, node.ymax
-
         if gu.intersecting(node.xmin, node.xmax, node.ymin, node.ymax,
                             interest['xmin'], interest['xmax'], interest['ymin'], interest['ymax']):
-            if verbose:
-                print "     intersection with leaf"
             if node.q1 == None:
                 for s in node.contents:
                     s_dist = gu.norm(s.ximg, s.yimg, x, y)
@@ -92,23 +83,11 @@ class Quadtree:
                         interest['ymax'] = y + s_dist
                         interest = gu.clip_box(interest['xmin'], interest['ymin'], interest['xmax'], interest['ymax'],
                                     tree.top.xmin, tree.top.ymin, tree.top.xmax, tree.top.ymax)
-                        if verbose:
-                            print "     -- new nearest: dist", s_dist, "box", interest['xmin'], interest['ymin'], interest['xmax'],
-                        interest['ymax']
-            if verbose:
-                print "\n"
             else:
-                if verbose:
-                    print "     intersection, checking children"
                 self.nearersource(tree, node.q1, x, y, nearest, interest)
                 self.nearersource(tree, node.q2, x, y, nearest, interest)
                 self.nearersource(tree, node.q3, x, y, nearest, interest)
                 self.nearersource(tree, node.q4, x, y, nearest, interest)
-        else:
-            if verbose:
-                "       no intersection"
-        if verbose:
-            print "Nearersource returning --",  nearest
 
 class Node:
     def __init__(self, xmin, ymin, xmax, ymax):
