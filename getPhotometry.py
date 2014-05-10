d3c7096861051510172804bd197f15863e4087cb
Alexa Villaume <avillaum@ucsc.edu>
2014-05-10 16:19:48 -0700

    added seeing iteration that cuts out extended and dim sources

--- getPhotometry.py
+++ getPhotometry.py
@@ -5,6 +5,7 @@ import sys
 import os
 from subprocess import call, Popen, PIPE
 from astropy.io import fits
+import glob
 
 import Sources as sources
 import createSexConfig
@@ -34,9 +35,10 @@ def calc_seeing(catalog):
     peak = phot_utils.detSizeCut(shape, 1000)
     ptsources = filter(lambda s: s.a_world <= peak, cat)
     ptsources = filter(lambda s: s.mag_aper <= 20, ptsources)
-
+    # Find the average FWHM and multiply it by the Suprime-Cam
+    # pixel scale to put it in arcsec
     fwhm = map(lambda line: line.fwhm, ptsources)
-    return sum(fwhm)/len(fwhm)
+    return sum(fwhm)/len(fwhm)*0.20
 
 def main():
     system = sys.argv[1]
@@ -54,19 +56,24 @@ def main():
     for i, img in enumerate(imgs):
         image = load_file(img, verbose=False)
         satur = image[0].header['SATURATE']
-#        seeing = 1
-#        ap = findBestAperture.findBestAperture(img, satur, seeing)
+        seeing = 1
+        ap = findBestAperture.findBestAperture(img, satur, seeing)
         fname = system + '_' + img[-6]
-#        # Extract sources with initial rough estimate of seeing
-#        config = createSexConfig.createSexConfig(fname, filter_file,
-#                 param_file, satur, seeing, "nill", ap, False)
-#        call(['sex', '-c', config, galsub[i], img])
+        # Extract sources with initial rough estimate of seeing
+        config = createSexConfig.createSexConfig(fname, filter_file,
+                 param_file, satur, seeing, "nill", ap, False)
+        call(['sex', '-c', config, galsub[i], img])
         seeing = calc_seeing(fname + '.cat')
 
         # Re-extract with refined seeing
         config = createSexConfig.createSexConfig(fname, filter_file,
                  param_file, satur, seeing, "nill", ap, False)
         call(['sex', '-c', config, galsub[i], img])
+
+        # Re-name the check images created
+        checks = (glob.glob('*.fits'))
+        for check in checks:
+            os.rename(check,
 
 if __name__ == '__main__':
     sys.exit(main())
