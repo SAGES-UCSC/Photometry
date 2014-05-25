15d7c308a5004a7c992b4ee3b206bf37e8b0dff1
Alexa Villaume <avillaum@ucsc.edu>
2014-05-24 22:01:57 -0700

    Added equatorial norm functions. Cleaned up comments

--- geom_utils.py
+++ geom_utils.py
@@ -1,16 +1,16 @@
 import math
 
-def calcY(x, m, b):
+def calc_y(x, m, b):
     y = m*x + b
     return y
 
-def inBox(x0, x1, y0, y1, px, py):
+def in_box(x0, x1, y0, y1, px, py):
     is_in = 0
     if px > x0 and px < x1 and py > y0 and py < y1:
         is_in = 1
     return is_in
 
-def inParallelogram(px, py,  m, b, var):
+def in_parallelogram(px, py,  m, b, var):
     is_in = 0
     y_top = calcY(px, m, b+var)
     y_bot = calcY(px, m, b-var)
@@ -18,18 +18,15 @@ def inParallelogram(px, py,  m, b, var):
         is_in = 1
     return is_in
 
-'''
-All of these inputs need to be in degrees.
-'''
-def makeRadiusCut(ra, dec, gal_ra, gal_dec, distance):
+def radius_cut(ra, dec, gal_ra, gal_dec, distance):
+    "All of these inputs need to be in degrees."
     if (math.sqrt((ra-gal_ra)**2) + math.sqrt((dec-gal_dec)**2)) <= distance:
         return 1
     else:
         return 0
 
-'''
-'''
 def intersecting(b1xmin, b1xmax, b1ymin, b1ymax, b2xmin, b2xmax, b2ymin, b2ymax):
+    "   "
     if (b1xmin >= b2xmin and b1xmin < b2xmax) or \
         (b1xmax >= b2xmin and b1xmax < b2xmax) or \
         (b1xmin <= b2ymin and b1xmax >= b2xmax) or \
@@ -44,19 +41,37 @@ def intersecting(b1xmin, b1xmax, b1ymin, b1ymax, b2xmin, b2xmax, b2ymin, b2ymax)
 
         return 0
 
-def norm(x1, y1, x2, y2):
-    return math.sqrt(norm2(x1, y1, x2, y2))
+def clip_box(bxmin, bymin, bxmax, bymax, boundsxmin, boundsymin, boundsxmax, boundsymax):
+    "   "
+    return {'xmin' : min(bxmin, boundsxmin), 'ymin' : min(bymin, boundsymin),
+            'xmax' : max(bxmax, boundsxmax), 'ymax' : max(bymax, boundsymax)}
 
-'''
-Do this to optimize for when you only care about relative distances
-'''
-def norm2(x1, y1, x2, y2):
+class norm(object):
+    """
+    Class and subfunctions to calculate the norm for
+    both pixel-coordinate objects and ra/dec objects
+    """
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, x1, y1, x2, y2):
+        return self.f(x1, y1, x2, y2)
+@norm
+def equnorm(x1, y1, x2, y2):
+    return math.sqrt(equnorm2(x1, y1, x2, y2))
+
+@norm
+def equnorm2(x1, y1, x2, y2):
+    a = ((x1-x2)*math.cos(y1))*((x1-x2)*math.cos(y1))
+    b = (y1-y2)*(y1-y2)
+    return a + b
+@norm
+def pixnorm(x1, y1, x2, y2):
+    return math.sqrt(pixnorm2(x1, y1, x2, y2))
+
+@norm
+def pixnorm2(x1, y1, x2, y2):
     xd = x2 - x1
     yd = y2 -y1
     return xd * xd + yd * yd
 
-'''
-'''
-def clip_box(bxmin, bymin, bxmax, bymax, boundsxmin, boundsymin, boundsxmax, boundsymax):
-    return {'xmin' : min(bxmin, boundsxmin), 'ymin' : min(bymin, boundsymin),
-            'xmax' : max(bxmax, boundsxmax), 'ymax' : max(bymax, boundsymax)}
