ebcd82159333baafecda6d9240d12112d8d8ad13
Alexa Villaume <avillaum@ucsc.edu>
2014-05-24 21:59:12 -0700

    Did some clean up. Nothing major changed

--- phot_utils.py
+++ phot_utils.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import Sources as sources
+from astropy.io import fits
 import geom_utils as gu
 
-# Convert from decimal to sexagesimal
-''' From http://idlastro.gsfc.nasa.gov/ftp/pro/astro/radec.pro '''
-''' Need to put better testing in '''
+
 def convertRA(ra):
+    """
+    Convert from decimal to sexagesimal
+    From http://idlastro.gsfc.nasa.gov/ftp/pro/astro/radec.pro
+    Need to put better testing in
+    """
     ra1 = math.trunc(ra/15.)
     ra2 = math.trunc((ra-ra1*15.)*4)
     ra3 = ((ra-ra1*15.-ra2/4.)*240.)
@@ -20,33 +25,33 @@ def convertDEC(dec):
 
     return str(dec1) + ':' + str(dec2) + ':' + str(round(dec3,6))
 
-# Distance should be in parsec
-def getDistMod(dist):
+def get_dist_mod(dist):
+    " Distance should be in parsec "
     return  5*math.log10(dist) - 5
 
-'''
-Add whatever correction, like a distance modulus, to a list of magnitudes
-This function takes an array to correct
-This isn't the right way to go about this...need to have the corrected mag
-be in the catalog
-'''
-def correctMag(c, correction):
+def correct_mag(c, correction):
+    '''
+    Add whatever correction, like a distance modulus,
+    to a list of magnitudes. This function takes an
+    array to correct
+    '''
     return map(lambda c: c + correction , c)
 
-'''
-Cut out noise, good for DSIM input files...
-Update this function to use filter() but be able to specify the element in the object
-'''
-def makeMagCut(mag, low, high):
+def mag_cut(mag, low, high):
+    """
+    Define a high and low value and flag the
+    values that fall within those boundaries
+    """
     isGood = 0
     if mag <= high and mag >= low:
         isGood = 1
     return isGood
 
-'''
-This might need some tweaking with different colors.
-'''
-def makeColorCut(mag1, mag2, mag3, mag4, x0, x1, m, b, var):
+def color_cut(mag1, mag2, mag3, mag4, x0, x1, m, b, var):
+    """
+    Make color cut on a catalog based on the equation
+    of a line
+    """
     y_11 = gu.calcY(x1, m, b+var) # Top-right corner
     y_00 = gu.calcY(x0, m, b-var) # Lower-left corner, creates box that encompases GC locus
 
@@ -55,57 +60,82 @@ def makeColorCut(mag1, mag2, mag3, mag4, x0, x1, m, b, var):
     if gu.inBox(x0, x1, y_00, y_11, px, py):
         return gu.inParallelogram(px, py,  m, b, var)
 
-'''
-Not sure how well this will work for other data sets.
-'''
-def detSizeCut(shape_col, bin_num):
+def det_size_cut(shape_col, bin_num):
+    """
+    Our size cuts are based on the thought that the
+    point sources of a catalog with be clustered in
+    a Gaussian distribution. This function is intended
+    to return the far right value of the distribution
+    in order to make the size cut.
+    """
     hist, bins = np.histogram(shape_col, bin_num)
     peak =  hist.argmax()
 
     return round(bins[peak+10], 4)
 
-'''
-This will will show a histogram of the input.
-'''
-def LookAtShapes(shape_col, bin_num):
+def make_histogram(shape_col, bin_num):
+    """
+    Use this to check distribution of sizes values
+    from SE catalogs
+    """
     hist, bins = np.histogram(shape_col, bin_num)
     width = 0.7 * (bins[1] - bins[0])
     center = (bins[:-1] + bins[1:]) / 2
     plt.bar(center, hist, align='center', width=width)
     plt.show()
 
-'''
-Calculate the median of an array
-'''
-def calcMedian(data):
-    data.sort()
+def calc_seeing(catalog, **kwargs):
+    """
+    Calculate the seeing of an image by taking a SE catalog
+    cutting out the extended and dim sources and averaging
+    the FWHM. Only for Subaru Suprime-Cam right now
+    """
+    pixel_scale = 0.20
+    with open(catalog, 'r') as f:
+        tmp = filter(lambda line: no_head(line), f)
+        cat = map(lambda line: sources.SCAMSource(line), tmp)
+    shape = map(lambda s: s.a_world, cat)
+    peak = detSizeCut(shape, 1000)
+    ptsources = filter(lambda s: s.a_world <= peak, cat)
+    ptsources = filter(lambda s: mag_cut(s, 0, 20), ptsources)
+    #if kwargs['makereg']:
+    fwhm = map(lambda line: line.fwhm, ptsources)
+    return sum(fwhm)/len(fwhm)*pixel_scale
 
+def calc_median(data):
+    data.sort()
     if len(data) % 2 == 1:
         return data[(len(data) + 1)/2 -1]
     else:
         lower = data[len(data)/2 -1]
         upper = data[len(data)/2]
-        return (float(lower + upper))/2
+    return (float(lower + upper))/2
 
-'''
-Calculate the median absolute deviation of an array
-'''
-def calcMAD(data):
-    med = calcMedian(data)
+def calc_MAD(data):
+    " Compute Median Absolute Deviation "
+    med = calc_median(data)
     tmp = []
     for val in data:
         tmp.append(math.fabs(val - med))
     tmp.sort()
-    return(calcMedian(tmp))
+    return(calc_median(tmp))
 
-'''
-Check for header on input files
-'''
-def noHead(line):
+def load_fits(f, **kwargs):
+    if kwargs['verbose']:
+        print ' ---- loading file ---- '
+    __hdulist = fits.open(f)
+    if kwargs['verbose']:
+        __hdulist.info()
+        __hdulist[0].data
+        __hdulist[0].data.shape
+        print ' ---- loaded file ---- '
+    return __hdulist
+
+def no_head(line):
+    " Tag commented lines for easy removal "
     if line[0] != "#":
         return True
     else:
         return False
-
 
 #def testColorCut():
