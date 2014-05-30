ce1a27fa6a8881299802cde6ff894c2c9c91b059
Alexa Villaume <avillaum@ucsc.edu>
2014-05-30 15:18:36 -0700

    Nearly there now, still some things hardcoded

--- calcZeropoint.py
+++ calcZeropoint.py
@@ -1,9 +1,3 @@
-'''
-PURPOSE
-    To calculate the zeropoint for an image
-INPUT
-    Aperture corrected values from the Subaru images
-'''
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -38,7 +32,6 @@ def getSDSS(galaxy):
     for i, entry in enumerate(result[1]):
         if entry['cl'] != 6:
             index.append(i)
-            #result[1].remove_row(i)
     result[1].remove_rows(index)
 
     # SDSS magnitudes are not exactly in AB so need to correct
@@ -49,22 +42,24 @@ def calcZP(sdss, scam):
     To calculate the zeropoint of the Subaru image match the Subaru catalog
     and the table returned from Vizier.
     """
-
     with open(scam, 'r') as catalog:
         tmp = filter(lambda line: phot_utils.no_head(line), catalog)
     # Do magnitude cute on data here
     tmp2 = map(lambda line: S.SCAMSource(line), tmp)
+    mag = map(lambda s: s.mag_best, tmp2)
+    max_mag = phot_utils.calc_average(mag) + 0.25*phot_utils.calc_average(phot_utils.variance(mag))
+    sources = filter(lambda s: phot_utils.mag_cut(s.mag_best, 0, max_mag), tmp2)
+
 
     ra = map(lambda line: line.ra, tmp2)
     dec = map(lambda line: line.dec, tmp2)
     scam_sources = Q.ScamEquatorialQuadtree(min(ra), min(dec),
                                             max(ra), max(dec))
-    map(lambda sources: scam_sources.insert(sources), tmp2)
+    map(lambda sources: scam_sources.insert(sources), sources)
     matches = associate(sdss, scam_sources)
 
     m_scam = map(lambda source: source.mag_aper, matches)
     m_sdss = map(lambda source: source.match2['gmag'], matches)
-
 
     # Clip outliers of (m_sdss - m_scam)
     difference = []
@@ -83,15 +78,14 @@ def calcZP(sdss, scam):
         difference.append(entry.match2['gmag'] - entry.mag_aper)
     m_scam = map(lambda source: source.mag_aper, clipped)
 
-    #plot to see
+    # Look at offsets
     plt.plot(difference, m_scam, linestyle='none', marker='o')
     plt.xlabel(r'$m_{SDSS}$ - $m_{SCAM}$', fontsize=20)
     plt.ylabel(r'$m_{SCAM}$', fontsize=20, labelpad=30)
-    #plt.tight_layout()
     plt.show()
-#
-#    # Take median of offset
-#    return phot_utils.calcMedian()
+
+    # Take median of offset
+    return  phot_utils.calcMedian(difference)
 
 def main():
     galaxy, scam_catalog = sys.argv[1], sys.argv[2]
