539baab8bc94ef98383b5d4bfce07110d58b1714
Alexa Villaume <avillaum@ucsc.edu>
2014-06-05 15:16:00 -0700

    uncommented out line 40

--- Examples/UCD_Photometry.py
+++ Examples/UCD_Photometry.py
@@ -37,8 +37,7 @@ def get_photometry(system, in_images):
         seeing = 1
         path = os.getcwd()
         fname = system + '_' + img[-12]
-        #ap = findBestAperture.findBestAperture(path, img, satur, seeing)
-        ap = 3.06
+        ap = findBestAperture.findBestAperture(path, img, satur, seeing)
         seeing = phot_utils.calc_seeing(fname + '.cat', verbose=verbose)
         "If the aperture is less than the seeing round it up to next interger"
         print seeing[1]
