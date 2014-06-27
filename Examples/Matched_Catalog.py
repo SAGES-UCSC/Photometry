70594269924ae2aa2caf3c0d0b09ca8101c5285a
Alexa Villaume <avillaum@ucsc.edu>
2014-06-27 15:01:19 -0700

    Updating Matched_Catalog

--- Examples/Matched_Catalog.py
+++ Examples/Matched_Catalog.py
@@ -41,23 +41,24 @@ def get_photometry(system, in_images):
     with(open(in_images, "r")) as f:
         for line in f:
             cols = line.split()
-            galsub.append(cols[0])
+            subs.append(cols[0])
             imgs.append(cols[1])
 
     filter_file = "default.conv"
     param_file = createSexParam.createSexParam(system, False)
 
-    for i, img in enumerate(imgs):
-        image = phot_utils.load_fits(img, verbose=False)
+    path = '/Users/alexawork/Documents/GlobularClusters/Data/NGC4621'
+    for galsub, img in zip(subs, imgs):
+        image = phot_utils.load_fits(path + img, verbose=False)
         path = os.getcwd()
-        fname = system + '_' + img[-12]
+        fname = system + '_' + img[-5]
         seeing = [1, 1]
         satur = image[0].header['SATURATE']
         ap = findBestAperture.findBestAperture(path, img, satur, seeing[0])
         # Extract sources with initial rough estimate of seeing
         config = createSexConfig.createSexConfig(fname, filter_file,
                  param_file, satur, seeing[0], "nill", ap, False)
-        call(['sex', '-c', config, galsub[i], img])
+        call(['sex', '-c', config, galsub, img])
 
         seeing = phot_utils.calc_seeing(fname + '.cat', verbose=verbose)
         "If the aperture is less than the seeing round it up to next interger"
@@ -66,7 +67,7 @@ def get_photometry(system, in_images):
         # Re-extract with refined seeing
         config = createSexConfig.createSexConfig(fname, filter_file,
                  param_file, satur, seeing[0], "nill", ap, False)
-        call(['sex', '-c', config, galsub[i], img])
+        call(['sex', '-c', config, galsub, img])
 
         # Re-name the check images created
         checks = (glob.glob('*.fits'))
