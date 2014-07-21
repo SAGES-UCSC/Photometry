fe3b9eb3772647d1b86058685318df3cbc034e26
Alexa Villaume <avillaum@ucsc.edu>
2014-07-21 00:07:41 -0500

    Removed clip_box because it is now redundant

--- geom_utils.py
+++ geom_utils.py
@@ -45,11 +45,6 @@ def intersecting(b1xmin, b1xmax, b1ymin,
 
         return 0
 
-def clip_box(bxmin, bymin, bxmax, bymax, boundsxmin, boundsymin, boundsxmax, boundsymax):
-    "   "
-    return {'xmin' : min(bxmin, boundsxmin), 'ymin' : min(bymin, boundsymin),
-            'xmax' : max(bxmax, boundsxmax), 'ymax' : max(bymax, boundsymax)}
-
 def equnorm(x1, y1, x2, y2):
     return math.sqrt(equnorm2(x1, y1, x2, y2))
 
