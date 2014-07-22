1fda364be1b680b54fef9c78aec6766c362950c5
Alexa Villaume <avillaum@ucsc.edu>
2014-07-21 22:21:12 -0500

    updated in_box

--- geom_utils.py
+++ geom_utils.py
@@ -7,10 +7,7 @@ def calc_y(x, m, b):
     return y
 
 def in_box(x0, x1, y0, y1, px, py):
-    is_in = 0
-    if px > x0 and px < x1 and py > y0 and py < y1:
-        is_in = 1
-    return is_in
+    return px >= x0 and px < x1 and py >= y0 and py < y1:
 
 def in_parallelogram(px, py,  m, b, var):
     is_in = 0
