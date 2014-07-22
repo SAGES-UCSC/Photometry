65c837aee0e4f54088bd3a74f50103a94956f5f6
Alexa Villaume <avillaum@ucsc.edu>
2014-07-22 02:45:41 -0500

    testing memoize functions

--- test.py
+++ test.py
@@ -1,13 +1,36 @@
-import random as r
-import Quadtree
-import Sources as S
-import phot_utils
+import profile
 
-sources = Quadtree.ScamPixelQuadtree(0, 0, 15000, 15000)
+class memoize:
+    # from http://avinashv.net/2008/04/python-decorators-syntactic-sugar/
+    def __init__(self, function):
+        self.function = function
+        self.memoized = {}
 
-with open("/Users/alexawork/Documents/GlobularClusters/Photometry/NGC4649/NGC4649_g.cat") as f:
-    tmp = filter(lambda line: phot_utils.no_head(line), f)
-map(lambda line: sources.insert(S.SCAMSource(line)), tmp)
+    def __call__(self, *args):
+        try:
+            return self.memoized[args]
+        except KeyError:
+            self.memoized[args] = self.function(*args)
+            return self.memoized[args]
 
-sources.match(10, 15)
-sources.debug()
+@memoize
+def fib(n):
+    # from http://en.literateprograms.org/Fibonacci_numbers_(Python)
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def fib_seq(n):
+    seq = [ ]
+    if n > 0:
+        seq.extend(fib_seq(n-1))
+    seq.append(fib(n))
+    return seq
+
+if __name__ == '__main__':
+    print 'MEMOIZED'
+    print '=' * 80
+    profile.run('print fib_seq(20); print')
