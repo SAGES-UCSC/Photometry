02fe2c076fd40a471cdeb11787559868653de887
Alexa Villaume <avillaum@ucsc.edu>
2014-07-24 13:10:56 -0400

    New way of reading and writing the catalog files. Untested.

---
+++ Catalogs.py
@@ -0,0 +1,53 @@
+"""
+A class to keep track of the methods for catalog files
+"""
+
+class ScamCatalog(object):
+    def __init__(self, fname):
+        self.data = open(fname, 'r')
+        line_count = 0
+        while True:
+            if line_count < 27:
+                self.data.readline()
+            else:
+                line = self.data.readline()
+                line = line.lstrip()
+                if line[0] == '#':
+                    break
+            line_count+=1
+
+        self.header = ["number", "flux_iso", "fluxerr_iso", "flux_aper", \
+                "fluxerr_aper", "ximg", "yimg", "ra", "dec", "mag_auto", "magerr_auto", \
+                "mag_best", "magerr_best", "mag_aper", "magerr_aper", "a_world", \
+                "a_world_err", "b_world", "b_world_err", "theta_err", "theta", "isoarea", \
+                "mu_max", "flux_radius", "flags", "fwhm", "elongation", "vignet"]
+
+    def next(self):
+        line = self.data.readline()
+        line = line.lstrip()
+        if line == "":
+            self.data.close()
+            raise StopIteration()
+
+        cols = line.split()
+        if len(cols) != len(self.header):
+            print "Input catalog is not valid."
+            raise StopIteration()
+
+        for element, col in zip(self.header, cols):
+            self.__dict__.update({element:float(col)})
+        self.__dict__.update({"match2":None})
+        self.__dict__.update({"match3":None})
+
+        return self.__dict__.copy()
+
+    def __iter__(self):
+        return self
+
+    # Need to figure out how to write out a catalog when match2 and
+    # mach3 != None
+    def catalog_write(self, outname):
+        with open(outname, "w") as out:
+            out.write("".join(self.header + "\n"))
+            for source in self:
+                out.write(''.join(str(element) for element in self)
